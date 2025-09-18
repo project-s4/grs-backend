264903216fc8c77d201b19779fd3a06c950210c6
rgsuhas <rgsuhas6364@gmail.com>
2025-09-18 23:53:12 +0530

    feat: Update database URL

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:password@localhost/db")
+DATABASE_URL = "postgresql+psycopg2://grievance_user:strongpassword@localhost:5432/grievance_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
